929297f45fbb4319380f901d4f683670a3ddaf88
amirreza <amir.rsf1380@gmail.com>
2023-12-17 21:11:16 +0330

    argon2

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,6 +119,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# password hashers
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
